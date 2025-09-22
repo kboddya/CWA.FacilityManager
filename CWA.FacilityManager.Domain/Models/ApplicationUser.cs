db864df52f3f5479f20736d2c2cc3baa49441dbb
kboddya <kboddya@gmail.com>
2025-09-22 10:05:37 +0300

    Init

---
+++ CWA.FacilityManager.Domain/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CWA.FacilityManager.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ CWA.FacilityManager.Infrastructure/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using CWA.FacilityManager.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CWA.FacilityManager.Infrastructure.Contexts
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
